018273dd9c76918d990b75a1a54beff95d1f40c0
Pascal Riquier <pascal.riquier@vlaanderen.be>
2020-01-07 11:22:14 +0100

    first commit

---
+++ vl-description-data.src.js
@@ -0,0 +1,19 @@
+import { VlElement, define } from '/node_modules/vl-ui-core/vl-core.js';
+
+/**
+ * VlDescriptionData
+ * @class
+ * @classdesc 
+ * 
+ * @extends VlElement
+ * 
+ * @property 
+ * 
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-description-data/releases/latest|Release notes}
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-description-data/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-description-data.html|Demo}
+ * 
+ */
+export class VlDescriptionData extends VlElement(HTMLElement) {}
+
+define('vl-description-data', VlDescriptionData);
