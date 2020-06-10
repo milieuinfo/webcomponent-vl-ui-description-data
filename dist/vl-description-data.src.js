3b2e4f09612fa79d56bd9ce86e03d0f276c7190d
Tom Coemans <tom.coemans@gmail.com>
2020-06-10 16:24:19 +0200

    dist cache

---
+++ dist/vl-description-data.src.js
@@ -0,0 +1,43 @@
+import { NativeVlElement, define } from 'vl-ui-core';
+
+/**
+ * VlDescriptionData
+ * @class
+ * @classdesc Gebruik de description data component om meer informatie te geven over de inhoud op de pagina, bvb over een contactpersoon, een entiteit of een publicatie.
+ * 
+ * @extends VlNativeElement
+ * 
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-description-data/releases/latest|Release notes}
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-description-data/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-description-data.html|Demo}
+ * 
+ */
+export class VlDescriptionData extends NativeVlElement(HTMLDivElement) {
+	constructor() {
+		super();
+		this.classList.add('vl-description-data');
+		this.__labels.forEach(label => {
+			label.classList.add("vl-description-data__label");
+			label.parentElement.classList.add("vl-description-data-block");
+		})
+		this.__values.forEach(label => {
+			label.classList.add("vl-description-data__value");
+			label.parentElement.classList.add("vl-description-data-block");
+		})
+	}	
+	
+	get __descriptionDataBlocks() {
+		return this.querySelectorAll(".vl-description-data-block");
+	}
+	
+	get __labels() {
+		return this.querySelectorAll("[data-vl-label]");
+	}
+
+	get __values() {
+		return this.querySelectorAll("[data-vl-value]");
+	}
+}
+
+define('vl-description-data', VlDescriptionData, {extends: 'div'});
+
